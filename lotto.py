c0bb7a10f8324fb63f3a8c741466fbaf7d65af3d
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-10-20 19:55:10 +0200

    tenyleg lotto

--- lotto.py
+++ lotto.py
@@ -3,7 +3,7 @@ szamok:list[int] = []
 
 futasok_szama = 0
 while len(szamok) < 5:
-    r = random.randint(1, 5)
+    r = random.randint(1, 90)
     if r not in szamok:
         szamok.append(r)
     futasok_szama += 1
